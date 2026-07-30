cf477c314926e9d4f353bb1b408d03b13f8036ee
alaa ebrahem <ebrahemalaa733@gmail.com>
2026-07-30 20:05:32 +0300

    Create AccountVerified notification

---
+++ app/Notifications/AccountVerified.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class AccountVerified extends Notification
+{
+    use Queueable;
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+
+            'type'=>'account_verified',
+
+            'message'=>
+                'تمت مراجعة مستنداتك والموافقة عليها'
+
+        ];
+    }
+}
