c07a518442701d1cbb7c860b35a7e3be8d72d5c0
alaa ebrahem <ebrahemalaa733@gmail.com>
2026-08-08 12:39:20 +0300

    Update API routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,11 @@ use App\Http\Controllers\Project\StepController;
 use App\Http\Controllers\ProviderController;
 // use App\Http\Controllers\DocumentController;
 // use App\Http\Controllers\NotificationController;
-// use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\Project\ProjectController;
+use App\Http\Controllers\Project\OfferController;
+use App\Http\Controllers\Project\ProjectInvitationController;
+use App\Http\Controllers\Project\ProjectReportController;
+use App\Http\Controllers\Interaction\RatingController;
 use App\Http\Controllers\SettingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -36,6 +40,26 @@ Route::get('/provinces', [SettingController::class, 'provinces']);
 //         Route::get('/notifications/unread-count',  'unreadCount');
 //         Route::patch('/notifications/markAsRead',  'markAsRead');
    });
+   Route::middleware('auth:sanctum')->group(function(){
+
+    Route::get('/projects',[ProjectController::class,'index']);
+    Route::get('/projects/{project}', [ProjectController::class, 'show']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+  Route::put('/projects/{project}', [ProjectController::class, 'update']);
+  Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
+
+});
+
+
+Route::prefix('admin')
+    ->middleware(['auth:sanctum', 'user.type:admin'])
+    ->group(function () {
+
+        Route::get('ratings', [RatingController::class, 'index']);
+        Route::get('ratings/{rating}', [RatingController::class, 'adminShow']);
+        Route::delete('ratings/{rating}', [RatingController::class, 'adminDestroy']);
+
+    });
 
 //     Route::get('getClients/{role}', [CustomerController::class, 'getClients']);
 //     Route::get('getSteps/{project}', [ProjectController::class, 'getSteps']);
@@ -47,10 +71,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/provider/private-tenders', [ProviderController::class, 'privateTenders']);
     Route::get('/provider/tenders/{id}', [ProviderController::class, 'showTender']);
     Route::post('/provider/invitations/{id}/decline',[ProviderController::class, 'declineInvitation']);
-    Route::get('/provider/offers',[ProviderController::class, 'myOffers']);
-    Route::post('/provider/projects/{project}/offers', [ProviderController::class, 'storeOffer']);
-    Route::put('/provider/offers/{offer}', [ProviderController::class, 'updateOffer']);
-    Route::delete('/provider/offers/{offer}', [ProviderController::class, 'deleteOffer']);
+
+    Route::get('/provider/offers',[OfferController::class, 'myOffers']);
+    Route::post('/provider/projects/{project}/offers', [OfferController::class, 'store']);
+    Route::put('/provider/offers/{offer}', [OfferController::class, 'update']);
+    Route::delete('/provider/offers/{offer}', [OfferController::class, 'destroy']);
+
     Route::get('/provider/projects', [ProviderController::class, 'projects']);
     Route::get('/provider/projects/{project}', [ProviderController::class, 'showProject']);
     Route::get('/provider/projects/{project}/tracking', [ProviderController::class, 'projectTracking']);
@@ -62,20 +88,51 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/provider/steps/{step}', [StepController::class, 'update']);
     Route::delete('/provider/steps/{step}', [StepController::class, 'destroy']);
 
+   Route::get('/provider/complaints', [ProviderController::class, 'complaints']);
+    
+   Route::get('/provider/invitations',[ProjectInvitationController::class, 'index']);
+   Route::get('/provider/invitations/{invitation}',[ProjectInvitationController::class, 'show']);
+   Route::post('/provider/invitations/{invitation}/accept',[ProjectInvitationController::class, 'accept']);
+   Route::post('/provider/invitations/{invitation}/decline',[ProjectInvitationController::class, 'decline']);
+
+   Route::post('/provider/projects/{project}/reports',[ProjectReportController::class, 'store']);
+   Route::get('/provider/projects/{project}/reports', [ProjectReportController::class, 'index']);
+   Route::get('/provider/projects/{project}/reports/{report}',[ProjectReportController::class, 'show']);
+   Route::put('provider/reports/{report}',[ProjectReportController::class, 'update']);
+   Route::delete('provider/reports/{report}', [ProjectReportController::class, 'destroy']);
+   
+    Route::get('provider/ratings',[RatingController::class, 'providerRatings']);
+   Route::get('provider/ratings/{rating}',[RatingController::class, 'providerShow']);
+
+
+
 });
 
     Route::middleware(['auth:sanctum', 'user.type:client'])->prefix('client')->group(function () {
     Route::get('projects',[ClientController::class, 'projects']);
     Route::get('projects/{project}',[ClientController::class, 'show']);
     Route::get('projects/{project}/offers',[ClientController::class, 'getOffers']);
-    Route::post('projects/{project}/offers/{offer}/accept',[ClientController::class, 'acceptOffer']);
-    Route::post('projects/{project}/offers/{offer}/reject',[ClientController::class, 'rejectOffer']);
+    Route::post('projects/{project}/offers/{offer}/accept',[OfferController::class, 'acceptOffer']);
+    Route::post('projects/{project}/offers/{offer}/reject',[OfferController::class, 'rejectOffer']);
     Route::post('projects/{project}/rate', [ClientController::class, 'rate']);
     Route::get('complaints', [ClientController::class, 'complaints']);
     Route::post('complaints',[ClientController::class, 'storeComplaint']);
     Route::get('projects/{project}/steps', [StepController::class, 'clientIndex']);
     Route::get('projects/{project}/steps/{step}', [StepController::class, 'clientShow']);
 
+
+    Route::post('projects/{project}/invitations',[ProjectInvitationController::class, 'store']);
+    Route::delete('invitations/{invitation}',[ProjectInvitationController::class, 'destroy']);
+    
+    Route::get( 'projects/{project}/reports',[ProjectReportController::class, 'clientIndex'] );
+    Route::get('projects/{project}/reports/{report}',[ProjectReportController::class, 'clientShow']);
+
+    Route::post('projects/{project}/ratings', [RatingController::class, 'store']);
+    Route::put('ratings/{rating}',[RatingController::class, 'update']);
+    Route::delete('ratings/{rating}',[RatingController::class, 'destroy']);
+    Route::get('ratings/{rating}',[RatingController::class, 'show']);
+    Route::get('my-ratings',[RatingController::class, 'myRatings']);
+ 
           });
 
 Route::middleware(['auth:sanctum', 'user.type:admin'])->prefix('admin')->group(function () {
