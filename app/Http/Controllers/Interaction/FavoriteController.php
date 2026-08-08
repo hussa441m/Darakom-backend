74fb75e42f676dd0bc811cae7be41bfba75ef0cd
Hussam Mohdur <kinghussam75@gmail.com>
2026-08-08 13:09:21 +0300

    feat: add FavoriteController to manage user favorites

---
+++ app/Http/Controllers/Interaction/FavoriteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Interaction;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FavoriteController extends Controller
+{
+    //
+}
