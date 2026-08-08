09bacbded341e23320892299533ee989acf07f02
Hussam Mohdur <kinghussam75@gmail.com>
2026-08-08 23:42:23 +0300

    Add API routes for forgot and reset password

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,8 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
 Route::get('/provinces', [SettingController::class, 'provinces']);
 
